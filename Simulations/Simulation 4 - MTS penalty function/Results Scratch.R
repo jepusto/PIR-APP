44a48b58f5ec5c2ef4d10c3f0448a05f2ce385ca
DMSwan <dms031000@gmail.com>
2015-02-16 15:53:19 -0600

    Small summary

---
+++ Simulations/Simulation 4 - MTS penalty function/Results Scratch.R
@@ -0,0 +1,16 @@
+ddply(.data = subset(results, stat = "phi"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat = "zeta"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat = "logit phi"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat = "log zeta"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat = "mu"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat = "lambda"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat = "log mu"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat = "log lambda"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
