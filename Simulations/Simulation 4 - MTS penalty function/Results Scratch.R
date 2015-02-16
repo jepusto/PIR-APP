b0dcd508f71e56301184a935e490426c5be09787
DMSwan <dms031000@gmail.com>
2015-02-16 16:02:07 -0600

    Slightly more info

--- Simulations/Simulation 4 - MTS penalty function/Results Scratch.R
+++ Simulations/Simulation 4 - MTS penalty function/Results Scratch.R
@@ -1,16 +1,24 @@
-ddply(.data = subset(results, stat = "phi"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
-ddply(.data = subset(results, stat = "zeta"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
-ddply(.data = subset(results, stat = "logit phi"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
-ddply(.data = subset(results, stat = "log zeta"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
-ddply(.data = subset(results, stat = "mu"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
-ddply(.data = subset(results, stat = "lambda"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
-ddply(.data = subset(results, stat = "log mu"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
-ddply(.data = subset(results, stat = "log lambda"), 
-      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias))
+ddply(.data = subset(results, stat == "phi"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
+ddply(.data = subset(results, stat == "zeta"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
+ddply(.data = subset(results, stat == "logit phi"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
+ddply(.data = subset(results, stat == "log zeta"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
+ddply(.data = subset(results, stat == "mu"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
+ddply(.data = subset(results, stat == "lambda"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
+ddply(.data = subset(results, stat == "log mu"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
+ddply(.data = subset(results, stat == "log lambda"), 
+      .variables = .(K_intervals, k_priors, theta), summarize, bias = mean(bias),
+      median_bias = mean(median_bias), variance = mean(var))
