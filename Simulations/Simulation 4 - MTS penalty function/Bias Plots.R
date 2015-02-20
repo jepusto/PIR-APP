5c97f3b3a7cbf7e2bf18c6553d79adec2335e8a2
James E. Pustejovsky <jepusto@gmail.com>
2015-02-19 21:28:56 -0600

    Tweaked bias plots to separate sample size.

--- Simulations/Simulation 4 - MTS penalty function/Bias Plots.R
+++ Simulations/Simulation 4 - MTS penalty function/Bias Plots.R
@@ -9,9 +9,9 @@ results_all <- rbind(results, results_unp)
 results_unp$theta <- 20
 results_all <- rbind(results_all, results_unp)
 
-results_all$k_pretty <- as.factor(with(results_all, ifelse(k_priors == 0, "Unpenalized", paste("k prior = ", k_priors))))
-
-results_all$k_pretty <- with(results_all, factor(k_pretty, levels = levels(k_pretty)[c(4,1:3)]))
+# results_all$k_pretty <- as.factor(with(results_all, ifelse(k_priors == 0, "Unpenalized", paste("k prior = ", k_priors))))
+# 
+# results_all$k_pretty <- with(results_all, factor(k_pretty, levels = levels(k_pretty)[c(4,1:3)]))
 
 simulation_plot <- function(data, statistic, graph = "bias",
                             breaks = c(-Inf, -1,-.50,-.15,-0.05,-0.02,0.02, 0.05,
@@ -28,7 +28,7 @@ simulation_plot <- function(data, statistic, graph = "bias",
     qplot(phi, zeta, fill = cuts,
         geom = "tile",
         data = data_sub) +
-    facet_grid(theta ~ k_pretty) +
+    facet_grid(K_intervals + theta ~ k_priors, labeller = "label_both") +
     scale_y_continuous(limits = c(.005, .60), breaks=c(.05, .10, .20, .30, .40, .50)) + 
     scale_x_continuous(limits = c(0.005, .60), breaks=c(.05, .10, .20, .30, .40, .50)) +
     scale_color_brewer(type = "seq") + labs(fill = "Bias") + theme_bw() + 
