5cd8af1532e8ff26f2c5aefb6edf4b7848be1822
DMSwan <dms031000@gmail.com>
2015-02-24 17:07:43 -0600

    Various comparison tables

--- Simulations/Simulation 4 - MTS penalty function/MC error and RMSE summaries.R
+++ Simulations/Simulation 4 - MTS penalty function/MC error and RMSE summaries.R
@@ -1,4 +1,5 @@
 library(plyr)
+library(reshape2)
 setwd("Simulations/Simulation 4 - MTS penalty function")
 rm(list = ls())
 load("sim4MTS_unp2.Rdata")
@@ -7,8 +8,6 @@ load("sim4MTSpenalized2.Rdata")
 results_unp2$theta <- 0
 results_unp2$k_priors <- 0
 results_all <- rbind(results2, results_unp2)
-
-head(results_all)
 
 mc_error <- function(data, statistic){
 
@@ -26,7 +25,7 @@ phi <- cbind(stat = "phi", mc_error(data = results_all, statistic = "phi"))
 lzeta <- cbind(stat = "log zeta", mc_error(data = results_all, statistic = "log zeta"))
 zeta <- cbind(stat = "zeta", mc_error(data = results_all, statistic = "zeta"))
 
-rbind(lphi, phi, lzeta, zeta)
+dcast(rbind(lphi, phi, lzeta, zeta), stat~K_intervals, value.var = "max_error")
 
 
 # median/max absolute bias
@@ -49,5 +48,17 @@ sim_summary <- function(data, statistic){
   
 }
 
-sim_summary(data = results_all, stat = "phi")
-sim_summary(data = results_all, stat = "zeta")
+phi_sum <- sim_summary(data = results_all, stat = "phi")
+zeta_sum <- sim_summary(data = results_all, stat = "zeta")
+
+#phi max bias, median bias, max RMSE and median RMSE in order
+dlply(phi_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "max_bias")[1:2]
+dlply(phi_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "median_bias")[1:2]
+dlply(phi_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "max_rmse")[1:2]
+dlply(phi_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "median_rmse")[1:2]
+
+#zeta max bias, median bias, max RMSE and median RMSE in order
+dlply(zeta_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "max_bias")[1:2]
+dlply(zeta_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "median_bias")[1:2]
+dlply(zeta_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "max_rmse")[1:2]
+dlply(zeta_sum, .(K_intervals), dcast, formula = k_priors ~ theta, value.var = "median_rmse")[1:2]
