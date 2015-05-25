a3b5dcc65375b2008f2dbf6252fada3dcf65afa0
DMSwan <dms031000@gmail.com>
2015-05-25 14:35:00 -0500

    Wood Memo Update

--- data/Wood scratch.R
+++ data/Wood scratch.R
@@ -1,7 +1,7 @@
-
 library(ARPobservation)
 library(ggplot2)
 library(plyr)
+library(reshape2)
 source("R/PIR-APP.R")
 source("R/parallel setup.R")
 wood <- read.csv("data/Wood et al data.csv", na.strings = ".", stringsAsFactors = FALSE)
@@ -14,7 +14,7 @@ PIR_wrap <- function(data_line, c = 1, d = 0, k_prior = 1, theta_prior = Inf){
   results <- PIRbootstrap(U = U, c = c, d = d, coding = data_line[3], 
          penalty_func = Beta_Gamma(k_mu = k_prior, k_lambda = k_prior, 
                                    theta_mu = theta_prior, 
-                                   theta_lambda = theta_prior), transform = "exp", iterations = 100)
+                                   theta_lambda = theta_prior), transform = "exp", iterations = 10)
   
   summary <- mean(U)
   
@@ -23,15 +23,32 @@ PIR_wrap <- function(data_line, c = 1, d = 0, k_prior = 1, theta_prior = Inf){
 }
 
 
-mle <- adply(wood, 1, PIR_wrap)
+source_func <- ls()
+cluster <- start_parallel(source_func)
+clusterEvalQ(cluster, library(ARPobservation))
 
-ple <- adply(wood, 1, PIR_wrap, k_prior = 1.5, theta_prior = 10)
+system.time(ple <- adply(wood, 1, PIR_wrap, k_prior = 1.5, theta_prior = 10, .parallel = TRUE))
 
-mle$estimator <- "mle"
-ple$estimator <- "ple"
+stopCluster(cluster)
 
-wood <- rbind(mle, ple)
+ple <- ple[,-(4:70)]
 
+cdr <- read.csv("data/transcribed.csv")[,-1]
+
+ple <- merge(ple, cdr)
+
+PIR <- subset(ple, Measurement.Type == "PIR")
+WIR <- subset(ple, Measurement.Type == "WIR")
+
+names(PIR)[5:9] <- paste0("PIR", names(PIR[5:9]))
+names(WIR)[5:9] <- paste0("WIR", names(WIR[5:9]))
+
+ple_wide <- merge(PIR[,-3],WIR[,-3])
+
+pl3 <- dcast(ple2, Classroom.ID + Child + parm ~ Measurement.Type, value.var = c("parm", "est", "sd", "CI_L", "CI_U"))
+
+
+head(ple)
 
 qplot(summary, est, ymin = CI_L, ymax = CI_U, color = factor(Measurement.Type), geom = "pointrange", data = subset(wood, parm == "phi" & estimator == "mle"))
 
